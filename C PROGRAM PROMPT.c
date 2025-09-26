61a1a9c7e0c3f974eb16ad2fe62731ee4ef4843f
KibeKelvin <kibekevlin@gmail.com>
2025-09-26 10:06:44 +0300

    Add files via upload

---
+++ C PROGRAM PROMPT.c
@@ -0,0 +1,26 @@
+//simple c program(Hello World)single line comment
+/*
+NAME:KIBE KELVIN
+REG NO:CT101/G/24702/24
+DATE:14/9/25
+DESCRIPTION:A PROGRAM TO PROMPT USER TO INPUT HEIGHT,BANK BALANCE AND PHONE NUMBER
+*/
+#include<stdio.h>
+//main function
+int main(){
+    int age;
+    printf("Enter your height(IN M):");
+    scanf("%h,&height\n");//take user input
+
+    printf("Enter your bank balance:\n");
+    scanf("%ksh,&bank balance\n");//take user input
+
+    printf("Enter phone number:\n");
+    scanf("%T,&phone number\n");//take user input
+
+    return 0;
+}
+
+
+
+
